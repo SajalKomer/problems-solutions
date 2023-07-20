81467ecf11d953897ff35b0b4ad0ace607e5c057
SajalKomer <sajalkomer@gmail.com>
2023-07-19 21:09:29 -0400

    add two more solutions

--- src/leetCode/RemoveElement.java
+++ src/leetCode/RemoveElement.java
@@ -1,11 +1,14 @@
 package leetCode;
 
 public class RemoveElement {
-//    public int removeElement(int[] nums, int val) {
-//        for (int i=0; i<nums.length; i++){
-//            if (nums[i]==val){
-//
-//            }
-//        }
-//    }
+    public int removeElement(int[] nums, int val) {
+        int insertIndex =0;
+        for (int j = 0; j < nums.length; j++){
+            if (nums[j] != val){
+                nums[insertIndex] = nums[j];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
 }
