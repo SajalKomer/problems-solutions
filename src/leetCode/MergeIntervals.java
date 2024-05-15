d60b876d06e90ed1842241f7f6995bf21ffdb207
SajalKomer <sajalkomer@gmail.com>
2024-05-15 13:41:16 -0400

    add one more solution

--- src/leetCode/MergeIntervals.java
+++ src/leetCode/MergeIntervals.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 /*
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 Example 1:
 Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
--- src/leetCode/NumberOfIslands.java
+++ src/leetCode/NumberOfIslands.java
@@ -1,7 +1,8 @@
 package leetCode;
 /*
-* Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
-An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+* Given an m x n 2D binary grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume
+ all four edges of the grid are all surrounded by water.
 
 Example 1:
 Input: grid = [
@@ -11,7 +12,7 @@ Input: grid = [
   ["0","0","0","0","0"]
 ]
 Output: 1
-*
+
 Example 2:
 Input: grid = [
   ["1","1","0","0","0"],
